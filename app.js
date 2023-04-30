35dcf2ff3f0e5e4474e99c790c9a1b502bb7212e
Ahmad Ali <ahmed.ali@mergestack.com>
2023-04-30 23:10:37 +0500

    Implementing monitor functionality 2

--- app.js
+++ app.js
@@ -1,47 +1,11 @@
 const express = require('express');
+const os = require('os');
+const pidusage = require('pidusage');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
-function formatDate(date) {
-  return Math.floor(date.getTime() / 1000);
-}
-
-
-
-async function monitor(k) {
-  if (isNaN(k)) {
-    console.error(`Error monitoring system resources: invalid input value for k`);
-    return null;
-  }
-
-  const now = new Date();
-  const startTime = new Date(now - k * 60 * 1000);
-
-  try {
-    const { stdout: cpuUsage } = await exec(`top -l 1 -F -R -o cpu -U $(whoami) -stats pid,command,cpu,time -s 5 -S ${formatDate(startTime)}`);
-    const { stdout: memoryUsage } = await exec(`top -l 1 -F -R -o mem -U $(whoami) -stats pid,command,mem,time -s 5 -S ${formatDate(startTime)}`);
-    
-    const cpuData = cpuUsage.trim().split('\n').slice(1).map(line => {
-      const [pid, command, cpu, time] = line.trim().split(/\s+/);
-      return { pid, command, cpu: parseFloat(cpu), time: parseFloat(time) };
-    });
-
-    const memoryData = memoryUsage.trim().split('\n').slice(1).map(line => {
-      const [pid, command, mem, time] = line.trim().split(/\s+/);
-      return { pid, command, mem: parseFloat(mem), time: parseFloat(time) };
-    });
-
-    const cpuUsageAvg = cpuData.reduce((acc, curr) => acc + curr.cpu, 0) / cpuData.length;
-    const memoryUsageAvg = memoryData.reduce((acc, curr) => acc + curr.mem, 0) / memoryData.length;
-
-    return { cpuUsage: `${cpuUsageAvg.toFixed(2)}%`, memoryUsage: `${memoryUsageAvg.toFixed(2)}%` };
-  } catch (err) {
-    console.error(`Error monitoring system resources: ${err.message}`);
-    return null;
-  }
-}
 
 let primeNumbers = [];
 
@@ -59,7 +23,12 @@ async function getPrimesInRange(start, end) {
       }
       
       if (isPrime && num > 1) {
+        console.log(num)
         primes.push(num);
+        if(!primeNumbers.includes(num))
+        {
+          primeNumbers.push(num);
+        }
       }
     }
     
@@ -75,8 +44,8 @@ app.post('/generate', async (req, res) => {
 
   // generate prime numbers in the background
   let primes = await getPrimesInRange(from, to);
-    console.log(43);
-    console.log(primes)
+    
+    
   res.json({ status: 'success' });
 });
 
@@ -87,17 +56,27 @@ app.get('/get', (req, res) => {
 
 // monitor endpoint
 app.post('/monitor', async (req, res) => {
-  
   const { k } = req.body;
-  const startTime = new Date();
-  const currentTime = new Date();
-  const elapsed = (currentTime - startTime) / 1000 / 60; // elapsed time in minutes
-  
-  const { cpuUsage, memoryUsage } = await monitor(k);
-  
-  res.json({ cpuUsage, memoryUsage });
+  const duration = k;
+  // Validate the input parameter
+  if (isNaN(duration)) {
+    return res.status(400).json({ error: 'Invalid input parameter' });
+  }
 
+  const now = Date.now();
+  const startTime = now - duration * 60 * 1000;
 
+  try {
+    const stats = await pidusage(process.pid, { startTime });
+    const memoryUsage = Math.round(stats.memory / os.totalmem() * 10000) / 100; // convert to percentage and round to 2 decimal places
+    const cpuUsage = Math.round(stats.cpu * 10000) / 100; // convert to percentage and round to 2 decimal places
+
+    res.json({ memoryUsage, cpuUsage });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching the usage data' });
+  }
 });
+
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
